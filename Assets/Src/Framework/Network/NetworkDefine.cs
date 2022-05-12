ff665757fe59b6ece53503bf8b188027f9749098
wuyiming <270949894@qq.com>
2022-05-12 17:29:02 +0800

    remove pbmessage.cs to src folder

--- Assets/Src/Framework/Network/NetworkDefine.cs
+++ Assets/Src/Framework/Network/NetworkDefine.cs
@@ -5,7 +5,7 @@ public sealed class NetworkDefine
 {
     public const string CHANNEL_NAME_GAME = "game";
     // 心跳间隔 s
-    public const int CHANEL_HEART_BRAT_INTERVAL = 3;
+    public const int CHANEL_HEART_BRAT_INTERVAL = 30;
 
     // 为了与 Bian.envelopType 区分开，从 10W + 开始计数
     public const int CHANEL_RANDOM_MIN_SEQ_ID = 100001;

24ec2c4d7c273121f64ba86a018d61be37a1f9f5
wuyiming <270949894@qq.com>
2022-05-13 14:00:40 +0800

    test code

--- Assets/Src/Framework/Network/NetworkDefine.cs
+++ Assets/Src/Framework/Network/NetworkDefine.cs
@@ -5,7 +5,7 @@ public sealed class NetworkDefine
 {
     public const string CHANNEL_NAME_GAME = "game";
     // 心跳间隔 s
-    public const int CHANEL_HEART_BRAT_INTERVAL = 3;
+    public const int CHANEL_HEART_BRAT_INTERVAL = 30;
 
     public const int CHANEL_RANDOM_MIN_SEQ_ID = 1;
     public const int CHANEL_RANDOM_MAX_SEQ_ID = 1000;

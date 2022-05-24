d4cffdf85539427ee4403d0405d81648ce625115
mangit <1048743890@qq.com>
2022-05-24 11:14:14 +0800

    add signplayer, entermap game action

---
+++ Assets/Src/Module/HttpAction/SigninPlayerAction.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+public class SigninPlayerAction : GameChannelNetMsgRActionBase<Bian.SigninPlayerRequest, Bian.SigninPlayerResponse>
+{
+    public static void Req(string playerID)
+    {
+        Bian.SigninPlayerRequest req = GenerateReq();
+        req.PlayerId = playerID;
+        req.ClientTime = Convert.ToInt32(Time.time);
+        req.IsDeveloper = true;
+        req.Chonglian = 0;
+        SendAction<SigninPlayerAction>(req);
+    }
+
+    protected override string GetEnvelopeReqName()
+    {
+        return "SigninPlayerRequest";
+    }
+
+    protected override Bian.EnvelopeType GetEnvelopeType()
+    {
+        return Bian.EnvelopeType.SigninPlayer;
+    }
+
+    protected override bool Receive(int errorCode, string errorMsg, Bian.SigninPlayerResponse rsp, Bian.SigninPlayerRequest req)
+    {
+        Message.SigninPlayerSuccess?.Invoke(rsp);
+        return true;
+    }
+}

105d59c23b67b34d5fb77b524a636ab5377ffb48
XQ <807929448@qq.com>
2022-05-26 16:33:23 +0800

    calculate camera dynamic view area

--- Assets/Src/Util/MathUtil.cs
+++ Assets/Src/Util/MathUtil.cs
@@ -1,20 +1,11 @@
 using System;
+using UnityEngine;
+
 /// <summary>
 /// 自定义的一些数学工具
 /// </summary>
 public static class MathUtil
 {
-    /// <summary>
-    /// 判断两个float相等
-    /// </summary>
-    /// <param name="a"></param>
-    /// <param name="b"></param>
-    /// <returns></returns>
-    public static bool FloatEquals(float a, float b)
-    {
-        return Math.Abs(a - b) < 0.0001f;
-    }
-
     /// <summary>
     /// 两个int转成一个ulong 方便将二维坐标转成一个key 类似对之前egret中的r_c的字符串优化
     /// </summary>
@@ -29,5 +20,23 @@ public static class MathUtil
     public static (int x, int y) UlongToTwoInt(ulong value)
     {
         return ((int)(value >> 32), (int)value);
+    }
+
+    /// <summary>
+    /// 和地面交点
+    /// </summary>
+    /// <param name="plane">地面高</param>
+    /// <returns></returns>
+    public static Vector3 GetPlaneInteractivePoint(Ray ray, float plane = 0)
+    {
+        Vector3 dir = ray.direction;
+
+        if (Mathf.Approximately(dir.y, 0))
+        {
+            return Vector3.zero;
+        }
+
+        float num = (plane - ray.origin.y) / dir.y;
+        return ray.origin + (ray.direction * num);
     }
 }

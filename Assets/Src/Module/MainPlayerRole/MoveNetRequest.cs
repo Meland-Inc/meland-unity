8c6a4c8fd63b3ebbc0b86802e7e904bbb2896fd6
XQ <807929448@qq.com>
2022-05-29 09:28:07 +0800

    use Teleport to move resolve map element not update

--- Assets/Src/Module/MainPlayerRole/MoveNetRequest.cs
+++ Assets/Src/Module/MainPlayerRole/MoveNetRequest.cs
@@ -16,7 +16,7 @@ public class MoveNetRequest : MonoBehaviour
         if (Time.realtimeSinceStartup - _lastReqTime > REQ_MOVE_INTERVAL)
         {
             _lastReqTime = Time.realtimeSinceStartup;
-            UpdateSelfLocationAction.Req(transform.position);
+            TeleportAction.Req(transform.position);//TODO:需要改成正式的移动协议  现在用移动协议 服务器会同步移动但是不会同步实体有问题
         }
     }
 }

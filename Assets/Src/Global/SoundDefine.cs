5df3aed13597abe5c8f3e0d3da3362f78d661e05
a412555466 <412555466@qq.com>
2022-06-13 11:56:20 +0800

    mr change

---
+++ Assets/Src/Global/SoundDefine.cs
@@ -0,0 +1,14 @@
+/*
+ * @Author: xiang huan
+ * @Date: 2022-06-13 11:46:22
+ * @LastEditTime: 2022-06-13 11:52:02
+ * @LastEditors: xiang huan
+ * @Description:
+ * @FilePath: /meland-unity/Assets/Src/Global/SoundDefine.cs
+ *
+ */
+public enum eSoundGroupType
+{
+    Music,
+    Sound,
+}

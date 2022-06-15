935e5346d53ee0437abaac966f09abb277550b30
a412555466 <412555466@qq.com>
2022-06-15 15:49:20 +0800

    add spineAnimCpt

---
+++ Assets/Src/Module/Animation/AnimationCpt.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * @Author: xiang huan
+ * @Date: 2022-06-14 19:07:39
+ * @LastEditTime: 2022-06-15 15:45:36
+ * @LastEditors: xiang huan
+ * @Description: 动画组件基类
+ * @FilePath: /meland-unity/Assets/Src/Module/Animation/AnimationCpt.cs
+ * 
+ */
+using System;
+using UnityEngine;
+
+public abstract class AnimationCpt : MonoBehaviour
+{
+    /// <summary>
+    /// 播放动画
+    /// </summary>
+    /// <param name="animationName">动画名</param>
+    /// <param name="loop">是否循环</param>
+    /// <param name="timeScale">播放速度</param>
+    public abstract void PlayAnim(string animationName, bool loop = false, float timeScale = 1f);
+
+    /// <summary>
+    /// 是否播放
+    /// </summary>
+    /// <param name="animationName">动画名</param>
+    public abstract bool IsPlaying(string animationName);
+    /// <summary>
+    /// 动画排队播放
+    /// </summary>
+    /// <param name="animationName">动画名</param>
+    /// <param name="loop">是否循环</param>
+    /// <param name="timeScale">播放速度</param>
+    /// <param name="delay">延长时间</param>
+    public abstract void PlayAnimQueued(string animationName, bool loop = false, float timeScale = 1f, float delay = 0f);
+    /// <summary>
+    /// 停止播放
+    /// </summary>
+    /// <param name="animationName">动画名</param>
+    public abstract void StopAnim(string animationName);
+    /// <summary>
+    /// 动画事件监听
+    /// </summary>
+    public Action<string, eAnimationEventType, object> EventDelegate = delegate { };
+}
+
+public enum eAnimationEventType
+{
+    /// <summary>
+    /// 动画开始
+    /// </summary>
+    Start,
+    /// <summary>
+    /// 动画停止
+    /// </summary>
+    End,
+    /// <summary>
+    /// 动画播放完成，如果是循环事件会重复出现多次
+    /// </summary>
+    Complete,
+    /// <summary>
+    /// 动画销毁
+    /// </summary>
+    Dispose,
+    /// <summary>
+    /// 用户自定义事件
+    /// </summary>
+    Event,
+}
+
+
---
+++ Assets/Src/Module/Animation/SpineAnimationCpt.cs
@@ -0,0 +1,181 @@
+using System.Collections.Generic;
+/*
+ * @Author: xiang huan
+ * @Date: 2022-06-14 14:11:43
+ * @LastEditTime: 2022-06-15 15:31:23
+ * @LastEditors: xiang huan
+ * @Description: spine动画组件
+ * @FilePath: /meland-unity/Assets/Src/Module/Animation/SpineAnimationCpt.cs
+ * 
+ */
+using System;
+using Spine.Unity;
+using Spine;
+
+public class SpineAnimationCpt : AnimationCpt
+{
+
+    private SkeletonAnimation _skeletonAnimation;
+    private Skeleton _skeleton;
+    private AnimationState _animationState;
+    private const int DEFAULT_TRACK_INDEX = 0;
+    private Dictionary<int, List<SpineAnimationInfo>> _animInfoMap;
+
+    public bool IsValid { get; private set; }
+    void Awake()
+    {
+        _animInfoMap = new();
+        IsValid = false;
+    }
+
+    public void Init()
+    {
+        if (IsValid)
+        {
+            return;
+        }
+        _skeletonAnimation = GetComponent<SkeletonAnimation>();
+        Init(_skeletonAnimation);
+    }
+
+    public void Init(object skeletonAnim)
+    {
+        if (IsValid)
+        {
+            return;
+        }
+        _skeletonAnimation = skeletonAnim as SkeletonAnimation;
+        if (_skeletonAnimation != null && _skeletonAnimation.valid)
+        {
+            IsValid = true;
+            _skeleton = _skeletonAnimation.skeleton;
+            _animationState = _skeletonAnimation.AnimationState;
+            _animationState.Start += OnSpineAnimationStart;
+            _animationState.Interrupt += OnSpineAnimationInterrupt;
+            _animationState.End += OnSpineAnimationEnd;
+            _animationState.Dispose += OnSpineAnimationDispose;
+            _animationState.Complete += OnSpineAnimationComplete;
+            ExecuteAnim();
+        }
+    }
+
+    public override void PlayAnim(string animationName, bool loop = false, float timeScale = 1f)
+    {
+        PlayAnim(DEFAULT_TRACK_INDEX, animationName, loop, timeScale);
+    }
+    public void PlayAnim(int trackIndex, string animationName, bool loop, float timeScale = 1f)
+    {
+        if (!IsValid)
+        {
+            SpineAnimationInfo info = SpineAnimationInfo.Create(trackIndex, animationName, loop, timeScale);
+            AddAnimationInfo(trackIndex, info, true);
+            return;
+        }
+        TrackEntry curEntry = _animationState.GetCurrent(trackIndex);
+        if (curEntry != null && curEntry.Animation.Name == animationName)
+        {
+            return;
+        }
+
+        TrackEntry entry = _animationState.SetAnimation(trackIndex, animationName, loop);
+        entry.TimeScale = timeScale;
+    }
+
+    public override void PlayAnimQueued(string animationName, bool loop = false, float timeScale = 1f, float delay = 0f)
+    {
+        PlayAnimQueued(DEFAULT_TRACK_INDEX, animationName, loop, delay, timeScale);
+    }
+    public void PlayAnimQueued(int trackIndex, string animationName, bool loop = false, float timeScale = 1f, float delay = 0f)
+    {
+        if (!IsValid)
+        {
+            SpineAnimationInfo info = SpineAnimationInfo.Create(trackIndex, animationName, loop, timeScale);
+            AddAnimationInfo(trackIndex, info, false);
+            return;
+        }
+        TrackEntry entry = _animationState.AddAnimation(trackIndex, animationName, loop, delay);
+        entry.TimeScale = timeScale;
+    }
+    private void AddAnimationInfo(int trackIndex, SpineAnimationInfo info, bool isClear)
+    {
+
+        if (_animInfoMap.TryGetValue(trackIndex, out List<SpineAnimationInfo> infoList))
+        {
+            if (isClear)
+            {
+                infoList.Clear();
+            }
+            infoList.Add(info);
+        }
+        else
+        {
+            infoList = new()
+            {
+                info
+            };
+            _animInfoMap.Add(trackIndex, infoList);
+        }
+    }
+    private void ExecuteAnim()
+    {
+        foreach (KeyValuePair<int, List<SpineAnimationInfo>> item in _animInfoMap)
+        {
+            for (int i = 0; i < item.Value.Count; i++)
+            {
+                SpineAnimationInfo info = item.Value[i];
+                PlayAnimQueued(item.Key, info.AnimationName, info.Loop, info.Delay, info.TimeScale);
+            }
+        }
+        _animInfoMap.Clear();
+    }
+    public override bool IsPlaying(string name)
+    {
+        return IsPlaying(DEFAULT_TRACK_INDEX, name);
+    }
+
+    public bool IsPlaying(int trackIndex, string name)
+    {
+        TrackEntry curEntry = _animationState.GetCurrent(trackIndex);
+        return curEntry != null && curEntry.Animation.Name == name;
+    }
+    public override void StopAnim(string name)
+    {
+        StopAnim(DEFAULT_TRACK_INDEX);
+    }
+
+    public void StopAnim(int trackIndex)
+    {
+        if (_animInfoMap.ContainsKey(trackIndex))
+        {
+            _ = _animInfoMap.Remove(trackIndex);
+        }
+        _animationState.ClearTrack(trackIndex);
+    }
+
+    public void OnSpineAnimationStart(TrackEntry trackEntry)
+    {
+        EventDelegate?.Invoke(trackEntry.Animation.Name, eAnimationEventType.Start, null);
+    }
+    public void OnSpineAnimationInterrupt(TrackEntry trackEntry)
+    {
+        EventDelegate?.Invoke(trackEntry.Animation.Name, eAnimationEventType.Complete, null);
+    }
+    public void OnSpineAnimationEnd(TrackEntry trackEntry)
+    {
+        EventDelegate?.Invoke(trackEntry.Animation.Name, eAnimationEventType.End, null);
+    }
+    public void OnSpineAnimationDispose(TrackEntry trackEntry)
+    {
+        EventDelegate?.Invoke(trackEntry.Animation.Name, eAnimationEventType.Dispose, null);
+    }
+    public void OnSpineAnimationComplete(TrackEntry trackEntry)
+    {
+        EventDelegate?.Invoke(trackEntry.Animation.Name, eAnimationEventType.Complete, null);
+    }
+
+    public void OnUserDefinedEvent(TrackEntry trackEntry, Event e)
+    {
+        EventDelegate?.Invoke(trackEntry.Animation.Name, eAnimationEventType.Event, e);
+    }
+
+}

2c375b82e4899b8d02b2e0e097786b7083eb48a2
mangit <1048743890@qq.com>
2022-06-20 20:26:31 +0800

    arrange code

---
+++ Assets/Src/Framework/UI/UICenter.Fgui.cs
@@ -0,0 +1,40 @@
+using System;
+/*
+ * @Author: mangit
+ * @LastEditTime: 2022-06-17 16:36:36
+ * @LastEditors: mangit
+ * @Description: fairygui 处理中心
+ * @Date: 2022-06-16 20:48:53
+ * @FilePath: /Assets/Src/Framework/UI/UICenter.Fgui.cs
+ */
+using FairyGUI;
+
+public partial class UICenter
+{
+    private readonly FguiExtensionCfg[] _extensionCfg = new FguiExtensionCfg[]
+    {
+        new (eFUIPackage.Backpack, FGUIDefine.NFT_ITEM_RES,typeof( BpNftItemRenderer)),
+    };
+    public static void InitPackage()
+    {
+        _ = UIPackage.AddPackage(UI_ASSET_PREFIX + eFUIPackage.Common.ToString());
+        _ = UIPackage.AddPackage(UI_ASSET_PREFIX + eFUIPackage.Backpack.ToString());
+    }
+
+    public void InitFguiExtension()
+    {
+        UIObjectFactory.SetLoaderExtension(typeof(MLoader));//扩展加载器加载资源方式
+        foreach (FguiExtensionCfg item in _extensionCfg)
+        {
+            try
+            {
+                string url = UIPackage.GetItemURL(item.Package.ToString(), item.ResName);
+                UIObjectFactory.SetPackageItemExtension(url, item.Type);
+            }
+            catch (Exception e)
+            {
+                MLog.Info(eLogTag.ui, $"extend fgui package item error: {e.Message}");
+            }
+        }
+    }
+}
--- Assets/Src/Framework/UI/UICenter.cs
+++ Assets/Src/Framework/UI/UICenter.cs
@@ -1,11 +1,11 @@
 using System.Collections.Generic;
 using FairyGUI;
 using UnityGameFramework.Runtime;
-public class UICenter : GameFrameworkComponent
+public partial class UICenter : GameFrameworkComponent
 {
     public const string FORM_ASSET_PREFIX = "Assets/Res/Prefab/UI/";
     public const string UI_ASSET_PREFIX = "Assets/Res/Fairygui/";
-    private Dictionary<string, int> _dicFormCacheID;
+    private Dictionary<string, List<int>> _dicFormCacheID;
     private void Start()
     {
         _dicFormCacheID = new();
@@ -17,26 +17,6 @@ public class UICenter : GameFrameworkComponent
     {
         DontDestroyOnLoad(Stage.inst.GetRenderCamera().gameObject);
         //init config
-    }
-
-    public static void InitPackage()
-    {
-        _ = UIPackage.AddPackage(UI_ASSET_PREFIX + eFUIPackage.Common.ToString());
-    }
-
-    public void InitFguiExtension()
-    {
-        UIObjectFactory.SetLoaderExtension(typeof(MLoader));//扩展加载器加载资源方式
-    }
-
-    public string GetFormAsset<T>() where T : FGUIForm, new()
-    {
-        return GetFormAsset(typeof(T).Name);
-    }
-
-    public string GetFormAsset(string formName)
-    {
-        return $"{FORM_ASSET_PREFIX}{formName}.prefab";
     }
 
     public int GetFormCacheID<T>() where T : FGUIForm, new()
@@ -56,9 +36,9 @@ public class UICenter : GameFrameworkComponent
 
     public int CheckFormCacheID(string assetName)
     {
-        if (_dicFormCacheID.TryGetValue(assetName, out int value))
+        if (_dicFormCacheID.TryGetValue(assetName, out List<int> value))
         {
-            return value;
+            return value[0];
         }
 
         return -1;
@@ -76,155 +56,30 @@ public class UICenter : GameFrameworkComponent
 
     public void SetFormCacheID(string formName, int serialID)
     {
+        if (!_dicFormCacheID.TryGetValue(formName, out List<int> value))
+        {
+            value = new List<int>();
+            _dicFormCacheID.Add(formName, value);
+        }
+        value.Add(serialID);
+    }
+
+    public void RemoveFromCacheID<T>() where T : FGUIForm, new()
+    {
+        RemoveFromCacheID(typeof(T).Name);
+    }
+
+    public void RemoveFromCacheID(string formName)
+    {
         if (_dicFormCacheID.ContainsKey(formName))
         {
-            MLog.Warning(eLogTag.ui, "form cache id is exist, formName: " + formName);
-            return;
+            List<int> value = _dicFormCacheID[formName];
+            value.Clear();
         }
-        _dicFormCacheID.Add(formName, serialID);
-    }
-
-    public static int OpenUIForm<T>() where T : FGUIForm, new()
-    {
-        return OpenUIForm<T>(eUIGroup.Form);
-    }
-
-    public static int OpenUIForm<T>(object userData) where T : FGUIForm, new()
-    {
-        return OpenUIForm<T>(eUIGroup.Form, userData);
-    }
-
-    public static int OpenUITooltip<T>() where T : FGUIForm, new()
-    {
-        return OpenUIForm<T>(eUIGroup.Tooltip);
-    }
-
-    public static int OpenUITooltip<T>(object userData) where T : FGUIForm, new()
-    {
-        return OpenUIForm<T>(eUIGroup.Tooltip, userData);
-    }
-
-    public static int OpenUIToast<T>() where T : FGUIForm, new()
-    {
-        return OpenUIForm<T>(eUIGroup.Toast);
-    }
-
-    public static int OpenUIToast<T>(object userData) where T : FGUIForm, new()
-    {
-        return OpenUIForm<T>(eUIGroup.Toast, userData);
-    }
-
-    public static int OpenUIAlert<T>() where T : FGUIForm, new()
-    {
-        return OpenUIForm<T>(eUIGroup.Alert);
-    }
-
-    public static int OpenUIAlert<T>(object userData) where T : FGUIForm, new()
-    {
-        return OpenUIForm<T>(eUIGroup.Alert, userData);
-    }
-
-    public static int OpenUIDialog<T>() where T : FGUIForm, new()
-    {
-        return OpenUIForm<T>(eUIGroup.Dialog);
-    }
-
-    public static int OpenUIDialog<T>(object userData) where T : FGUIForm, new()
-    {
-        return OpenUIForm<T>(eUIGroup.Dialog, userData);
-    }
-
-    public static int OpenUILog<T>() where T : FGUIForm, new()
-    {
-        return OpenUIForm<T>(eUIGroup.Log);
-    }
-
-    public static int OpenUILog<T>(object userData) where T : FGUIForm, new()
-    {
-        return OpenUIForm<T>(eUIGroup.Log, userData);
-    }
-
-    /// <summary>
-    /// 通过窗体class name打开窗体
-    /// </summary>
-    /// <param name="uiCom"></param>
-    /// <param name="formID">窗体的枚举ID</param>
-    /// <param name="groupName">窗体所在的组</param>
-    /// <param name="userData">传递给窗体的数据</param>
-    /// <typeparam name="T">窗体class</typeparam> 
-    /// <returns>int 窗体的序列号</returns>
-    public static int OpenUIForm<T>(eUIGroup group) where T : FGUIForm, new()
-    {
-        return OpenUIForm<T>(group, null);
-    }
-
-    /// <summary>
-    /// 通过窗体class name打开窗体
-    /// </summary>
-    /// <param name="uiCom"></param>
-    /// <param name="formID">窗体的枚举ID</param>
-    /// <param name="groupName">窗体所在的组</param>
-    /// <param name="userData">传递给窗体的数据</param>
-    /// <typeparam name="T">窗体class</typeparam> 
-    /// <returns>int 窗体的序列号</returns>
-    public static int OpenUIForm<T>(eUIGroup group, object userData) where T : FGUIForm, new()
-    {
-        string assetName = BasicModule.UICenter.GetFormAsset<T>();
-        if (string.IsNullOrEmpty(assetName))
+        else
         {
-            MLog.Error(eLogTag.ui, "asset name is empty,formID: " + assetName);
-            return -1;
+            MLog.Error(eLogTag.ui, "can't find form cache id,assetName: " + formName);
         }
-
-        int cacheID = BasicModule.UICenter.CheckFormCacheID(assetName);
-        if (cacheID != -1)
-        {
-            MLog.Warning(eLogTag.ui, "open form repeatedly, formName: " + typeof(T).Name);
-            return cacheID;
-        }
-
-        UIComponent uiCom = GFEntry.UI;
-        string groupName = group.ToString();
-        if (!uiCom.HasUIGroup(groupName))
-        {
-            bool addGroup = uiCom.AddUIGroup(groupName, (int)group);
-            if (!addGroup)
-            {
-                MLog.Error(eLogTag.ui, "add group failed,groupName: " + groupName);
-                return -1;
-            }
-        }
-
-        int serialID = uiCom.OpenUIForm(assetName, groupName, userData);
-        BasicModule.UICenter.SetFormCacheID<T>(serialID);
-        return serialID;
-    }
-
-    /// <summary>
-    /// 通过窗体class name关闭窗体
-    /// </summary>
-    /// <param name="uiCom"></param>
-    /// <typeparam name="T">窗体class</typeparam>
-    public static void CloseUIForm<T>() where T : FGUIForm, new()
-    {
-        int serialID = BasicModule.UICenter.GetFormCacheID<T>();
-        CloseUIForm(serialID);
-    }
-
-    public static void CloseUIForm(int serialID)
-    {
-        if (serialID == -1)
-        {
-            MLog.Error(eLogTag.ui, $"can't find form cache id,formID:{serialID}");
-            return;
-        }
-        GFEntry.UI.CloseUIForm(serialID);
-    }
-
-    public static T GetUIForm<T>() where T : FGUIForm, new()
-    {
-        int serialID = BasicModule.UICenter.GetFormCacheID<T>();
-        return GFEntry.UI.GetUIForm(serialID) as T;
     }
 }
 

5ab7a6058f72aee0b15a8f59cad2877e6cf3834e
mangit <1048743890@qq.com>
2022-06-21 16:24:02 +0800

    fix:add deleted items id

--- Assets/Src/Module/Network/Channel/GameChannel/Action/TItemDelAction.cs
+++ Assets/Src/Module/Network/Channel/GameChannel/Action/TItemDelAction.cs
@@ -2,7 +2,7 @@ using System;
 using System.Security.Cryptography;
 /*
  * @Author: mangit
- * @LastEditTime: 2022-06-16 20:07:28
+ * @LastEditTime: 2022-06-21 16:05:39
  * @LastEditors: mangit
  * @Description: 
  * @Date: 2022-06-16 20:00:33
@@ -24,6 +24,10 @@ public class TItemDelAction : GameChannelNetMsgTActionBase<TItemDelResponse>
         }
 
         string[] idList = new string[rsp.Items.Count];
+        for (int i = 0; i < rsp.Items.Count; i++)
+        {
+            idList[i] = rsp.Items[i].Id;
+        }
         DataManager.Backpack.RemoveData(idList);
         return true;
     }

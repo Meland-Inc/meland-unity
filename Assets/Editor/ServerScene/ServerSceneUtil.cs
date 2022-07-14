17f5764b08da83c5bc8ef2557653a401b96bafd6
a412555466 <412555466@qq.com>
2022-07-14 14:25:25 +0800

    add navmesh tool

---
+++ Assets/Editor/ServerScene/ServerSceneUtil.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * @Author: xiang huan
+ * @Date: 2022-07-12 15:17:07
+ * @Description: 服务器场景工具
+ * @FilePath: /meland-unity/Assets/Editor/ServerScene/ServerSceneUtil.cs
+ * 
+ */
+using UnityEngine;
+
+namespace Meland.Editor.ServerScene
+{
+    public sealed class ServerSceneUtil
+    {
+        public static void DestroyObjectComponent(GameObject gameObject)
+        {
+            //倒序删除 (避免RequireComponent的依赖导致无法删除)
+            Component[] components = gameObject.GetComponents<Component>();
+            for (int i = components.Length - 1; i >= 0; i--)
+            {
+                Component comp = components[i];
+                if (!(comp is Transform) && !(comp is Collider))
+                {
+                    Object.DestroyImmediate(comp, true);
+                }
+            }
+
+            int childCount = gameObject.transform.childCount;
+            for (int i = 0; i < childCount; i++)
+            {
+                Transform childTransform = gameObject.transform.GetChild(i);
+                DestroyObjectComponent(childTransform.gameObject);
+            }
+
+        }
+    }
+}

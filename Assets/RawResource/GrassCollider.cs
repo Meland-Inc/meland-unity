fb976602873c6f56f204c059b089a021efb90ea3
mangit <1048743890@qq.com>
2022-07-14 15:09:58 +0800

    fix:animation bug

--- Assets/RawResource/GrassCollider.cs
+++ Assets/RawResource/GrassCollider.cs
@@ -1,13 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Spine.Unity;
 
 public class GrassCollider : MonoBehaviour
 {
 
-    public SkeletonAnimation skeletonAnimation;
-    public AnimationReferenceAsset idle,run;
+    public SkeletonAnimation SkeletonAnimation;
+    public AnimationReferenceAsset Idle;
+    public AnimationReferenceAsset Run;
     public string currentState;    
 
     // Start is called before the first frame update
@@ -16,35 +15,22 @@ public class GrassCollider : MonoBehaviour
         SetAnimationState("idle");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(currentState!= "run"){
-            SetAnimationState("idle");
-        }
-    }
-
     public void OnTriggerEnter(){
         SetAnimationState("run");
     }
 
     public void SetAnimationState(string state){
         if(state.Equals("idle")){
-            SetAnimation(idle,true,1f);
+            SetAnimation(Idle, true, 1f);
         }else if(state.Equals("run")){
-            SetAnimation(run,true,1f);
+            SetAnimation(Run, true, 1f);
         }
-        currentState = state;
     }
 
     public void SetAnimation(AnimationReferenceAsset animation,bool loop,float timeScale){
-        if(animation!=null){
-
-
-            if(animation.name.ToLower().Contains(currentState.ToLower()) && !animation.name.Equals("hited") && !animation.name.Equals("attack")){
-                return;
-            }
-            Spine.TrackEntry animationEntry = skeletonAnimation.state.SetAnimation(0,animation,loop);
+        if (animation != null)
+        {
+            Spine.TrackEntry animationEntry = SkeletonAnimation.state.SetAnimation(0, animation, loop);
             animationEntry.TimeScale = timeScale;
             animationEntry.Complete += AnimationEntryComplete;
             currentState = animation.name;
--- Assets/RawResource/TreeIdle.cs
+++ Assets/RawResource/TreeIdle.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Spine.Unity;
 
@@ -8,44 +6,29 @@ public class TreeIdle : MonoBehaviour
 
     public SkeletonAnimation skeletonAnimation;
     public AnimationReferenceAsset idle;
-    public string currentState;    
+    public string CurrentState;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         SetAnimationState("idle");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetAnimationState(string state)
     {
-
-    }
-
-
-    public void SetAnimationState(string state){
-        if(state.Equals("idle")){
-            SetAnimation(idle,true,1f);
+        if (state.Equals("idle"))
+        {
+            SetAnimation(idle, true, 1f);
         }
-        currentState = state;
     }
 
-    public void SetAnimation(AnimationReferenceAsset animation,bool loop,float timeScale){
-        if(animation!=null){
-
-            if(animation.name.ToLower().Contains(currentState.ToLower()) && !animation.name.Equals("hited") && !animation.name.Equals("attack")){
-                return;
-            }
-            Spine.TrackEntry animationEntry = skeletonAnimation.state.SetAnimation(0,animation,loop);
+    public void SetAnimation(AnimationReferenceAsset animation, bool loop, float timeScale)
+    {
+        if (animation != null)
+        {
+            Spine.TrackEntry animationEntry = skeletonAnimation.state.SetAnimation(0, animation, loop);
             animationEntry.TimeScale = timeScale;
-            animationEntry.Complete += AnimationEntryComplete;
-            currentState = animation.name;
-        }
-    }
-
-    private void AnimationEntryComplete(Spine.TrackEntry trackEntry){
-        if(currentState.Equals("run")||currentState.Equals("run1")){
-            SetAnimationState("idle");
+            CurrentState = animation.name;
         }
     }
 }
